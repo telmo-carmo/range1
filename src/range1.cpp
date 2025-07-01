18f0922cbffaa2dcbbaa1e1917f2c0b1e8afe1fc
Telmo Carmo <telmo.carmo@gmail.com>
2025-07-01 17:01:51 +0100

    test span

--- src/range1.cpp
+++ src/range1.cpp
@@ -7,17 +7,18 @@
 int main(int argc, char* argv[]) {
     std::cout << "Range example with C++ version: " << __cplusplus << '\n';
     std::vector<int> a{1, 2, 3, 4, 5, 6 };
-    std::vector<char> b{'a', 'b', 'c', 'd'};
+  
 
     auto even = [](int x) { return x % 2 == 0; };
     // Transform: square the numbers
     auto square = [](int x) { return x * x; };
 
-    auto v2 = a | rg::views::filter(even) | rg::views::transform(square);
+    auto evens_squared = a | rg::views::filter(even) | rg::views::transform(square);
     int idx = 0;
-    for (int x : v2 ) {
+    for (int x : evens_squared ) {
         std::cout <<  tfmt::format("idx {} = {}\n", idx++,x);
     }
+
 
     ts::span<int> sp1(a); 
     auto process_range_part = [](ts::span<const int> s_part) {
@@ -26,9 +27,9 @@ int main(int argc, char* argv[]) {
             std::cout << val << " ";
         }
         std::cout << "\n";
-    };
+    };  
 
-    process_range_part(sp1.subspan(0, 4)); 
+    process_range_part(sp1.subspan(0, 4).first(3)); // Take first 3 elements from the first 4
 
     return 0;
 }
